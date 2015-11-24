63a72610ddda10cb39ad87c954abdcffc5bb3f24
Michael Cavalea <callmecavs@gmail.com>
2015-11-23 20:12:56 -0500

    add paths and names

--- outset-lib.js
+++ outset-lib.js
@@ -1,3 +1,17 @@
 #!/usr/bin/env node
 
 'use strict'
+
+const fs = require('fs')
+
+// cache paths
+const CWD = process.cwd()
+const FROM = __dirname + '/lib';
+
+// build names from cmd line arg
+let name = process.argv[2]
+
+const NAMES = {
+  lower: name.toLowerCase(),
+  upper: name.charAt(0).toUpperCase() + name.slice(1)
+}
