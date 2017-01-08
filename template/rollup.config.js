117613c92fe2895bc65af98480a79cdc671017dc
callmecavs <callmecavs@gmail.com>
2017-01-08 12:19:21 -0500

    start rollup config

--- template/rollup.config.js
+++ template/rollup.config.js
@@ -1,5 +1,23 @@
-const config = {
+import babel from 'rollup-plugin-babel'
+import babelrc from 'babelrc-rollup'
 
+const info = require('./package.json')
+
+const config = {
+  entry: 'lib/${ NAME.lower }.js'.
+  plugins: [ babel(babelrc()) ],
+  targets: [
+    {
+      dest: info.main,
+      format: 'umd',
+      moduleName: '${ NAME.lower }',
+      sourceMap: true
+    }, {
+      dest: info.module,
+      format: 'es',
+      sourceMap: true
+    }
+  ]
 }
 
 export default config
